32888901b66135b98fb14efe6bfe4007c34a0610
Ofer <Oferikad@gmail.com>
2020-12-03 12:49:50 +0100

    dict works-tho not sure if meaningful

--- migratin_analysis.R
+++ migratin_analysis.R
@@ -1,47 +1,53 @@
 #load packages
 library(readr)
 library(quanteda)
+library(quanteda.textmodels)
 library(readtext)
 library(tidyverse)
+library(data.table)
 
 
 #dictionary for sentiment analysis
 #https://www.rdocumentation.org/packages/quanteda/versions/2.1.2/topics/data_dictionary_LSD2015
 
+#load data
+Corp_HouseOfCommons_V2  <- readRDS("~/Desktop/tada-hoc/Corp_HouseOfCommons_V2.rds")
+names(Corp_HouseOfCommons_V2)
+
 #load and arragne dictionary
 dict_img <- read_csv("immigration_500.csv")
 dict_img <- dict_img %>% 
   subset(select = -c(`100.0`))
+#pivot wider
 dict_img <- dict_img %>%
-  pivot_wider(names_from= needs, values_from = needs)
+  pivot_wider(names_from= needs, values_from = needs, values_fn = list)
 rownames(dict_img ) <- c("immigration")
 #convert dictionary df to a list
-dict_list <- lapply(split(dict_img, row.names(dict_img)), unlist)
+dict_list <- lapply(dict_img, unlist)
+
 #turn to dictionary 
-dict <- dictionary(dict_list)
+dict_img <- dictionary(dict_list)
 
-#load data
-Corp_HouseOfCommons_V2  <- readRDS("~/Desktop/FP_textasdata/Corp_HouseOfCommons_V2.rds")
-
-#subset to speeches from 2006
+#subset to speeches from 2010 (Justification:Tory manifesto)
 speeches <- Corp_HouseOfCommons_V2 %>% 
   select(!c(iso3country, party.facts.id, parliament)) %>%
-  filter(date>"2005-12-20")
+  filter(date>"2009-12-20")
 
 #turn into corpus
 speechcorp <- corpus(speeches)
 
-#restrict to speeches with over 50 words
-speechcorp <- corpus_subset(speechcorp, ntoken(speechcorp) > 50)
-
 #summary
 summary(speechcorp)
 
+#restrict to speeches with over 50 words
+#clean_corp <- corpus_subset(speechcorp, ntoken(speechcorp) > 100)
+
 #dfm creates a document feature matrix
-img_dfm <- dfm(speechcorp, dictionary = dict)
+img_dfm <- dfm(speechcorp, dictionary = dict_img)
+
+#see which words appeared most
+textplot_wordcloud(img_dfm, max_words = 100, color = c('blue','yellow', 'green', 'orange'))
 
 #convert data frame because it is easier to work with
-#img_df <- convert(img_dfm, to = "data.frame")
-#head(img_df)
-
-
+img_df <- convert(img_dfm, to = "data.frame")
+head(img_df)

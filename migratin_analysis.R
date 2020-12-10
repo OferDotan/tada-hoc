cb5d89c6f273179dbbdb09862466557b8beef1f3
janpanhuysen <75311373+janpanhuysen@users.noreply.github.com>
2020-12-10 14:16:22 +0100

    Added and updated Sentiment part

--- migratin_analysis.R
+++ migratin_analysis.R
@@ -163,42 +163,33 @@ ggplot(count_months, aes(x=month, y=frequency))+
 
 
 
+#############################
+## SENTIMENT | Data Frames ##
+#############################
 
-
-#############
-# Sentiment #
-#############
-# Amir: This one might be more relevant to us: http://www.snsoroka.com/data-lexicoder/ 
-# although it is actually designed for use on political speech (often related to migration) [but] on social meda...
-# for reference, see https://www.tandfonline.com/doi/full/10.1080/1369183X.2019.1665990 (section 3.2. Measuring the sentiment of migration discourse on Facebook)
-# for another description of what the Lexicoder does, see https://ijoc.org/index.php/ijoc/article/view/6273/1946, page 975
-
-# how does sentiment evolve over time? 
-## H1: Overall, sentiments towards immigration became more negative, peaking in 2015 & 2016 
-# sentiment by party
-# sentrement (line graph) 
-# migrants as passive "bystanders" vs. active 
-
-
+# sentiment based on overall corpus
 sentiment <- textstat_polarity(speechcorp, 
                                data_dictionary_LSD2015)
-
-hist(sentiment$sentiment, breaks = 60)
 sentiment$sent_prob <- 1/(1 + exp(-sentiment$sentiment))
 
-#compare with kwic corpus
+hist(sentiment$sentiment, breaks = 60) # just for ourselves to see what's what
+
+# sentiment based on kwic corpus
 
 kwic_sentiment <- textstat_polarity(corp_kwic, 
-                               data_dictionary_LSD2015)
-
-hist(kwic_sentiment$sentiment, breaks = 60)
+                                    data_dictionary_LSD2015)
 kwic_sentiment$sent_prob <- 1/(1 + exp(-kwic_sentiment$sentiment))
+hist(kwic_sentiment$sentiment, breaks = 60) # just for ourselves to see what's what
 
 # add sentiments to initial data frames
 agenda_text_filter = cbind(agenda_text_filter,sentiment)
 kw_immigration = cbind(kw_immigration,kwic_sentiment)
 
-# graph sentiment over time for overall subset
+########################
+## SENTIMENT | GRAPHS ##
+########################
+
+## OVERALL sentiment
 
 agenda_text_filter$date <- as.Date(agenda_text_filter$date, format="%Y-%m-%d")
 sentiment_df <- data.frame(date = agenda_text_filter$date, sentiment = agenda_text_filter$sentiment)
@@ -208,38 +199,65 @@ sentiment_month = sentiment_df %>% group_by(month=floor_date(date, "month")) %>%
 ggplot(sentiment_month, aes(x=month, y=avg_sentiment))+
   geom_area( fill="#69b3a2", alpha=0.4, aes(group=1)) +
   geom_line(color="#69b3a2",aes(group=1)) +
-  ggtitle("Observed Sentiment in immigration related contributions overall")+
+  labs(title = "Observed Sentiment in immigration related contributions overall",
+       caption = "dashed line (black = 2015 general election, red = Brexit referendum")+
   geom_vline(xintercept = as.Date("2015-05-07"), linetype = "dashed")+ #general election 2015
   geom_vline(xintercept = as.Date("2016-06-23"), linetype = "dashed", color = "red")+ # Brexit referendum
   theme(axis.text.x = element_text(angle = 90))
 
-# graph sentiment over time for kwic subset ## currently no date for kwic
+## OVERALL sentiment by party
 
-#check if doc_id is unique
-length(unique(agenda_text_filter$doc_id)) # yes. 
+agenda_text_filter$date <- as.Date(agenda_text_filter$date, format="%Y-%m-%d")
+sentiment_party_df <- data.frame(date = agenda_text_filter$date, party = agenda_text_filter$party ,sentiment = agenda_text_filter$sentiment)
+sentiment_party = sentiment_party_df %>% group_by(month=floor_date(date, "month"), party = party) %>% summarise(avg_sentiment = mean(sentiment))
 
-# add date column to kwic
-doc_id_date <- data.frame(date = agenda_text_filter$date,docname = agenda_text_filter$doc_id) 
+ggplot(sentiment_party)+
+  geom_line(aes(x=month, y=avg_sentiment,group = party), colour = "grey",size = 1) +
+  geom_line(data = subset(sentiment_party, party == "Con") ,aes(x=month, y=avg_sentiment,group = party), colour = "red",size = 1) +
+  geom_line(data = subset(sentiment_party, party == "Lab") ,aes(x=month, y=avg_sentiment,group = party), colour = "blue",size = 1) +
+  labs(title = "Observed Sentiment in immigration related contributions overall by party",
+       subtitle = "Conservative = red, Labour = blue",
+       caption = "dashed line (black = 2015 general election, red = Brexit referendum")+
+  geom_vline(xintercept = as.Date("2015-05-07"), linetype = "dashed")+ #general election 2015
+  geom_vline(xintercept = as.Date("2016-06-23"), linetype = "dashed", color = "red")+ # Brexit referendum
+  theme(axis.text.x = element_text(angle = 90))
+
+
+## KWIC sentiment
+
+length(unique(agenda_text_filter$doc_id)) # for merge, check if doc_id is unique --> yes. 
+doc_id_date <- data.frame(date = agenda_text_filter$date,docname = agenda_text_filter$doc_id, party = agenda_text_filter$party) # add date column to kwic
 kw_imm_date <- merge(kw_immigration,doc_id_date,by = "docname", all.x = TRUE, sort = FALSE) # will need to merge based on the docname, as this is the same as the initial doc_id.
-
 
 kw_imm_date$date <- as.Date(kw_imm_date$date, format="%Y-%m-%d")
 kw_sentiment_df <- data.frame(date = kw_imm_date$date, sentiment = kw_imm_date$sentiment)
 kw_sentiment_month = kw_sentiment_df %>% group_by(month=floor_date(date, "month")) %>% summarise(avg_sentiment = mean(sentiment))
 
-
 ggplot(kw_sentiment_month, aes(x=month, y=avg_sentiment))+
   geom_area( fill="#69b3a2", alpha=0.4, aes(group=1)) +
   geom_line(color="#69b3a2",aes(group=1)) +
-  ggtitle("Observed Sentiment in Context of Keywords in immigration related contributions")+
+  labs(title = "Observed Sentiment in Context of Keywords in immigration related contributions",
+       caption = "dashed line (black = 2015 general election, red = Brexit referendum")+
   geom_vline(xintercept = as.Date("2015-05-07"), linetype = "dashed")+ #general election 2015
   geom_vline(xintercept = as.Date("2016-06-23"), linetype = "dashed", color = "red")+ # Brexit referendum
   theme(axis.text.x = element_text(angle = 90))
 
+## KWIC sentiment by party
 
+kw_imm_date$date <- as.Date(kw_imm_date$date, format="%Y-%m-%d")
+kw_sentiment__party_df <- data.frame(date = kw_imm_date$date, party = kw_imm_date$party,sentiment = kw_imm_date$sentiment)
+kw_sentiment_party = kw_sentiment__party_df %>% group_by(month=floor_date(date, "month"), party = party) %>% summarise(avg_sentiment = mean(sentiment))
 
-
-
+ggplot(kw_sentiment_party)+
+  geom_line(aes(x=month, y=avg_sentiment,group = party), colour = "grey",size = 1) +
+  geom_line(data = subset(kw_sentiment_party, party == "Con") ,aes(x=month, y=avg_sentiment,group = party), colour = "red",size = 1) +
+  geom_line(data = subset(kw_sentiment_party, party == "Lab") ,aes(x=month, y=avg_sentiment,group = party), colour = "blue",size = 1) +
+  labs(title = "Observed Sentiment in in context of keyword immigration related contributions by party",
+       subtitle = "Conservative = red, Labour = blue",
+       caption = "dashed line (black = 2015 general election, red = Brexit referendum")+
+  geom_vline(xintercept = as.Date("2015-05-07"), linetype = "dashed")+ #general election 2015
+  geom_vline(xintercept = as.Date("2016-06-23"), linetype = "dashed", color = "red")+ # Brexit referendum
+  theme(axis.text.x = element_text(angle = 90))
 
 
 
@@ -285,6 +303,22 @@ Corp_HouseOfCommons_V2$agenda["migration"]
 #test1 = with(speeches, speeches[ grepl( 'immigra*|Immigra*', agenda), ])
 
 #test2 = with(speeches, speeches[ grepl( 'immigra*|Immigra*|refugee*|Refugee*|asylum|Asylum', agenda), ])
+
+####### references for sentiment
+
+
+# how does sentiment evolve over time? 
+## H1: Overall, sentiments towards immigration became more negative, peaking in 2015 & 2016 
+# sentiment by party
+# sentrement (line graph) 
+# migrants as passive "bystanders" vs. active 
+
+# Amir: This one might be more relevant to us: http://www.snsoroka.com/data-lexicoder/ 
+# although it is actually designed for use on political speech (often related to migration) [but] on social meda...
+# for reference, see https://www.tandfonline.com/doi/full/10.1080/1369183X.2019.1665990 (section 3.2. Measuring the sentiment of migration discourse on Facebook)
+# for another description of what the Lexicoder does, see https://ijoc.org/index.php/ijoc/article/view/6273/1946, page 975
+
+
 
 
 
